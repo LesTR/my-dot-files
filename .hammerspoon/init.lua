736ef4f8b757080aeda93c50ab87b21a4e9dc168
Lukas Drbal <lukas.drbal@gmail.com>
2021-11-25 16:40:42 +0100

    Hammerspoon config

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,217 @@
+-----------------------------------------------
+-- Set up
+-----------------------------------------------
+
+local hyper = {"cmd", "ctrl"}
+hs.window.animationDuration = 0
+
+require("hs.application")
+require("hs.window")
+
+-----------------------------------------------
+-- hyper j for top half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'j', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper k for bottom half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'k', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper h for left one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'h', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper l for right one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'l', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper m for fullscreen
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'm', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w
+        f.h = max.h
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper u for top left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'u', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper p for top right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'p', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper . for bottom left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, '.', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x + (max.w / 2)
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- hyper n for bottom right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'n', function()
+    if hs.window.focusedWindow() then
+        local win = hs.window.focusedWindow()
+        local f = win:frame()
+        local screen = win:screen()
+        local max = screen:frame()
+
+        f.x = max.x
+        f.y = max.y + (max.h / 2)
+        f.w = max.w / 2
+        f.h = max.h / 2
+        win:setFrame(f)
+    else
+        hs.alert.show("No active window")
+    end
+end)
+
+-----------------------------------------------
+-- Reload config on write
+-----------------------------------------------
+
+function reload_config(files)
+    hs.reload()
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
+hs.alert.show("Config loaded")
+
+-----------------------------------------------
+-- Hyper i to show window hints
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'i', function()
+    hs.hints.windowHints()
+end)
+
